93123ec8a8ff426733898304c71020c6f5ff9f85
Guillaume <guillaumemengelatte@hotmail.fr>
2021-10-31 00:22:09 +0200

    close burger on children clicj

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const btn = document.querySelector('.menu_button');
-const navA = document.querySelector('.nav_a');
+let btn = document.querySelector('.menu_button');
+let navA = document.querySelector('.nav_a');
 
 btn.addEventListener('click',() => {
     navA.classList.toggle('show')
 })
+
+
+for (i = 0; i < navA.children.length; i++) {
+    navA.children[i].addEventListener('click',() => {
+        navA.classList.remove('show')
+    }) 
+}
